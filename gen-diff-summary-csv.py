5759477b8ffecfa6f5f0048bb1450b2582d2a083
soonoh <soonoh.jung@gmail.com>
2023-01-12 19:30:45 +0900

    Add module column

--- gen-diff-summary-csv.py
+++ gen-diff-summary-csv.py
@@ -17,7 +17,7 @@ for line in sys.stdin:
 
 # create csv file
 writer = csv.writer(sys.stdout)
-writer.writerow(["full path", "file name", "commit message"])
+writer.writerow(["full path", "file name", "commit message", "module"])
 
 for file_path in added_files + modified_files:
     # extract file name from path
@@ -26,4 +26,9 @@ for file_path in added_files + modified_files:
     commit_message = subprocess.run(["git", "log", "-1", "--pretty=%B", file_path], capture_output=True, text=True).stdout.strip()
     # cut commit message if it's longer than 80 characters
     commit_message = commit_message[:80] + "..." if len(commit_message) > 80 else commit_message
-    writer.writerow([file_path, file_name, commit_message])
+    # extract first path of file and last word of that path
+    if '/' in file_path:
+        module = file_path.split("/")[0] + "/" + file_path.split("/")[-2]
+    else:
+        module = ""
+    writer.writerow([file_path, file_name, commit_message, module])
