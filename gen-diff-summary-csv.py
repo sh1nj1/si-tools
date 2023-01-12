261cfad15314488776c45d2e099ada5baa862951
Soonoh Jung <tjfql08@cj.net>
2023-01-12 20:56:50 +0900

    Add remove commit messge prefix and deploy date column

--- gen-diff-summary-csv.py
+++ gen-diff-summary-csv.py
@@ -2,6 +2,14 @@ import csv
 import re
 import subprocess
 import sys
+import re
+from datetime import datetime
+
+DELIMITER=','
+ENCODING='utf-8-sig'
+
+sys.stdout.reconfigure(encoding = ENCODING)
+today = datetime.today().strftime('%Y-%m-%d')
 
 modified_files = []
 added_files = []
@@ -16,8 +24,8 @@ for line in sys.stdin:
         modified_files.append(line.split("--- a/")[1].strip())
 
 # create csv file
-writer = csv.writer(sys.stdout)
-writer.writerow(["module", "full path", "file name", "commit message"])
+writer = csv.writer(sys.stdout, delimiter = DELIMITER, quoting = csv.QUOTE_ALL)
+writer.writerow(["module", "full path", "file name", "commit message", "deploy date"])
 
 for file_path in added_files + modified_files:
     # extract file name from path
@@ -26,9 +34,11 @@ for file_path in added_files + modified_files:
     commit_message = subprocess.run(["git", "log", "-1", "--pretty=%B", file_path], capture_output=True, text=True).stdout.strip()
     # cut commit message if it's longer than 80 characters
     commit_message = commit_message[:80] + "..." if len(commit_message) > 80 else commit_message
+    # remove some commit message prefix
+    commit_message = re.sub(r"^[0-9]+\s+.+:\s+", "", commit_message)
     # extract first path of file and last word of that path
     if '/' in file_path and '-' in file_path:
         module = file_path.split("/")[0].split("-")[-1]
     else:
         module = ""
-    writer.writerow([module, file_path, file_name, commit_message])
+    writer.writerow([module, file_path, file_name, commit_message, today])
