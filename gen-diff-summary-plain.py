76c8a92ee80743b0554037538c062080b175a408
Soonoh Jung <soonoh.jung@gmail.com>
2023-01-31 01:51:27 +0900

    마크다운 포캣 출력 스크립트 추가

---
+++ gen-diff-summary-plain.py
@@ -0,0 +1,50 @@
+import csv
+import os
+import subprocess
+import sys
+import re
+import requests
+from datetime import datetime
+
+stdin = open(sys.argv[1]).readlines() if len(sys.argv) > 1 else sys.stdin
+
+DELIMITER=','
+ENCODING='utf-8-sig'
+COLUMNS = (os.getenv("COLUMNS") or "module,full path, filename, deploy date").split(",")
+
+
+sys.stdout.reconfigure(encoding = ENCODING)
+today = datetime.today().strftime('%Y-%m-%d')
+
+modified_files = []
+added_files = []
+
+def remove_duplicates(arr):
+    for i in range(len(arr), 1, -1):
+        if arr[i-1] in arr[:i-1]:
+            arr.pop(i-1)
+    return arr
+
+# parse git diff file
+for line in stdin:
+    if line.startswith("+++ b/"):
+        # extract added file path
+        added_files.append(line.split("+++ b/")[1].strip())
+    elif line.startswith("--- a/"):
+        # extract modified file path
+        modified_files.append(line.split("--- a/")[1].strip())
+
+writer = sys.stdout
+no = 0
+for file_path in remove_duplicates(added_files + modified_files):
+    if 'src/test' in file_path:
+        continue
+    # get last commit message for file
+    commit_message = subprocess.run(["git", "log", "-1", "--pretty=%B", file_path], capture_output=True, text=True).stdout.strip()
+    # cut commit message if it's longer than 80 characters
+    commit_message = commit_message[:80] + "..." if len(commit_message) > 80 else commit_message
+    # remove some commit message prefix
+    commit_message = re.sub(r"^[0-9]+\s+.+:\s+", "", commit_message)
+    no += 1
+    writer.write(f'{no}. {file_path}\n')
+    writer.write(f' - {commit_message}\n')
